423208eb27265b139be60d90351e7b1acd17fec7
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-05-10 15:18:53 -0400

    Completed studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                ViewBag.error = "Error: passwords do not match.";
+                return View("Add");
+            }
+        }
+    }
+}
